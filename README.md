55b6553a31c06d685708634231b40c07cf9196db
Massimo Cetra <mcetra@gmail.com>
2013-06-17 14:44:52 +0200

    Initial content.

--- README.md
+++ README.md
@@ -1,4 +1,107 @@
-gatwaay-rewrite
+gateway-rewrite
 ===============
 
 Node.js middleware to execute CGI scripts with URL rewriting
+
+/(Modified from gateway by Felix Gnass and added url rewriting capabilities)/
+
+Purpose of this module is to allow development tools like
+[Yeoman](http://yeoman.io) or [Livestyle](https://github.com/One-com/livestyle)
+to serve PHP files (and possibly other scripting languages) directly, simulating apache url rewriting.
+This is particularly useful when using PHP and Slim library in company with grunt-server
+
+
+To make this work you need the `php-cgi` binaray in your PATH.
+
+## Usage
+
+```javascript
+var http = require('http');
+var gateway_rw = require('gateway-rewrite');
+
+var rwGateway = function (dir){
+    return gateway_rw(require('path').resolve(dir), {
+       rules: [
+          {
+            rule: '^(/api/.+)',
+            cgi:  '/usr/bin/php-cgi',
+            to:   '/api/index.php'
+          }
+        ]},
+        grunt
+    );
+  };
+
+
+  grunt.initConfig({
+
+   (more here)
+
+    connect: {
+        options: {
+          hostname: 'localhost',
+          port: 9000
+        },
+        livereload: {
+          options: {
+            middleware: function (connect, options) {
+                return [
+                    /* Let's see if the rule matches something to rewrite */
+                    rwGateway(sysConfig.build_dir),
+                    /* Otherwise it's a static file */
+                    mountFolder(connect, sysConfig.build_dir)
+                ];
+            }
+          }
+        }
+    },
+  });
+
+
+```
+
+## Installing php-cgi
+
+My preferred way is to use APT and install the php5-cgi package.
+
+Alternatively, the `php-cgi` binary can be installed via Homebrew by tapping the
+[homebrew-php](https://github.com/josegonzalez/homebrew-php) repository:
+
+    brew tap homebrew/dupes
+    brew tap josegonzalez/homebrew-php
+    brew install php54
+
+
+## Directories
+
+if the request URI matches the regular expression *rule*, then the *cgi* binary is used
+rewriting to the PHP file in the *to* option.
+
+The *rules* option is an array so you can add as many rewrite rules you need.
+
+## License
+
+(The MIT License)
+
+Copyright (c) 2013 Massimo Cetra
+
+Thanks to Felix Gnass for the inspiration.
+
+Permission is hereby granted, free of charge, to any person obtaining
+a copy of this software and associated documentation files (the
+'Software'), to deal in the Software without restriction, including
+without limitation the rights to use, copy, modify, merge, publish,
+distribute, sublicense, and/or sell copies of the Software, and to
+permit persons to whom the Software is furnished to do so, subject to
+the following conditions:
+
+The above copyright notice and this permission notice shall be
+included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
---
+++ gateway-rewrite.js
@@ -0,0 +1,165 @@
+'use strict';
+
+var fs = require('fs'),
+    URL = require('url'),
+    path = require('path'),
+    extname = path.extname,
+    normalize = path.normalize,
+    join = path.join,
+    spawn = require('child_process').spawn,
+    statusExp = /^Status:\s*(\d{3}) (.*)$/i
+
+
+module.exports = function gateway_rewrite(docroot, options, grunt) {
+
+    // docroot is required
+    if (!docroot) {
+        throw new Error('gateway_rewrite() requires docroot')
+    }
+
+    // default mappings
+    options = options || {}
+    options.rules = options.rules || [];
+
+    return function (req, res, next) {
+        var exit, statusCode, reason, body;
+
+        function error(code, reason, headers) {
+            res.writeHead(code || 500, reason, headers)
+            res.end()
+        }
+
+        function done() {
+            if (exit === undefined) {
+                return
+            }
+
+            if (exit && !body) {
+                error(500, handler + ' exited with code ' + exit)
+            }
+            else {
+                res.end()
+            }
+        }
+
+        var matches = 0;
+        var url = URL.parse(req.url)
+
+        req.pause()
+
+        for (var j = 0; j < options.rules.length && !matches; j++) {
+            var rule = options.rules[j].rule;
+            var re = new RegExp(rule);
+
+            if (url.pathname.match(re)) {
+                matches ++;
+                var handler = options.rules[j].cgi
+                var file = options.rules[j].to
+                var uri = url.pathname
+                var path = normalize(join(docroot, file))
+
+                // populate the environment
+                var host = (req.headers.host || '').split(':')
+                var env = {
+                    SERVER_ROOT: docroot,
+                    DOCUMENT_ROOT: docroot,
+                    SERVER_NAME: host[0],
+                    SERVER_PORT: host[1] || 80,
+                    HTTPS: req.connection.encrypted ? 'On' : 'Off',
+                    REDIRECT_STATUS: 200,
+
+                    SCRIPT_NAME: file,
+                    REQUEST_URI: uri,
+                    SCRIPT_FILENAME: path,
+                    PATH_TRANSLATED: path,
+                    REQUEST_METHOD: req.method,
+                    QUERY_STRING: url.query || '',
+                    GATEWAY_INTERFACE: 'CGI/1.1',
+                    SERVER_PROTOCOL: 'HTTP/1.1',
+                    PATH: process.env.PATH,
+                    __proto__: options.env || {},
+
+                    REMOTE_ADDR: '127.0.0.1' // Fake
+                }
+
+                // expose request headers
+                for (var header in req.headers) {
+                    var name = 'HTTP_' + header.toUpperCase().replace(/-/g, '_')
+                    env[name] = req.headers[header]
+                }
+
+                if ('content-length' in req.headers) {
+                    env.CONTENT_LENGTH = req.headers['content-length']
+                }
+
+                if ('content-type' in req.headers) {
+                    env.CONTENT_TYPE = req.headers['content-type']
+                }
+
+
+                var child = spawn(handler, [], {
+                    'env': env
+                }).on('exit', function (code) {
+                    exit = code
+                    done()
+                })
+
+                var line = []
+
+                child.stdout.on('end', done).on('data', function (buf) {
+
+                    if (body) {
+                        return res.write(buf)
+                    }
+
+                    for (var i = 0; i < buf.length; i++) {
+
+                        var c = buf[i]
+
+                        if (c == 0xA) {
+                            if (!line.length) {
+                                body = true
+                                res.writeHead(statusCode || 200, reason)
+                                res.write(buf.slice(i + 1))
+                                return
+                            }
+
+                            var s = line.join('')
+                            line = []
+                            if (!statusCode) {
+                                var m = statusExp.exec(s)
+                                if (m) {
+                                    statusCode = m[1]
+                                    reason = m[2]
+                                    continue;
+                                }
+                            }
+
+                            var idx = s.indexOf(':')
+                            if ( !body ) {
+                            }
+                                res.setHeader(s.slice(0, idx), s.slice(idx + 1).trim())
+                        } else if (c != 0xD) {
+                            line.push(String.fromCharCode(c))
+                        }
+                    }
+
+                })
+
+                req.pipe(child.stdin)
+            }
+        }
+
+        req.resume()
+
+        if (options.stderr) {
+            child.stderr.pipe(options.stderr)
+        }
+
+        if ( matches == 0 ) {
+            return next();
+        }
+
+    }
+}
+
